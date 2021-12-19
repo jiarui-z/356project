d07652c437d3dd60cbc7f9c499cb84c5091515ae
Jiarui Zhang <j879zhan@uwaterloo.ca>
2021-12-19 15:14:57 -0500

    add more course and subjects related query

--- edu.py
+++ edu.py
@@ -8,10 +8,10 @@ class DB:
         self.connection = mysql.connector.connect(
             host='marmoset04.shoshin.uwaterloo.ca',
             user='h93zheng',
-            password='dblW65zwrxUfMg3OHR%4'
+            password='dblW65zwrxUfMg3OHR%4',
+            database='db356_h93zheng'
         )
         self.cursor = self.connection.cursor()
-        self.exec('use db356_h93zheng')
 
     def exec(self, query, params=None):
         self.cursor.execute(query, params)
@@ -25,7 +25,7 @@ class DB:
 
 
 class Education(cmd.Cmd):
-    intro = 'Type help to show all available commands.\n'
+    intro = 'Type help to show all available commands.\nType q to exit the program.\n'
     prompt = '(edu) '
 
     def __init__(self):
@@ -33,17 +33,58 @@ class Education(cmd.Cmd):
         self.db = DB()
 
     def do_check_course_offering(self, arg):
-        return
+        'check if a course is offered in a term'
+
+        course_name = input('Enter the course name: ')
+        term = input('Enter the term code: ')
+        try:
+            query = f'''
+            select
+                count(1)
+            from
+                Courses As C
+                inner join CourseOfferings AS CO on C.uuid = CO.course_uuid
+                where C.name = '{course_name}' and CO.term_code = '{term}';
+            '''
+            res = self.db.exec(query).fetchall()[0][0]
+            if res:
+                print(f'{course_name} is offered in {term}')
+            else:
+                print(f'{course_name} is not offered in {term}')
+        except mysql.connector.Error as error:
+            print(f'Check course offering failed with error: {error}')
 
     def do_get_course_offering_by_subject(self, arg):
+        'get all course offerings in a subject'
+
+        subject = input('Enter the subject code: ')
+
+        try:
+            query = f'''
+            select
+                distinct CO.name
+            from
+                Subjects As S
+                inner join SubjectMemberships AS SM on S.code = SM.subject_code
+                inner join CourseOfferings AS CO on SM.course_offering_uuid = CO.uuid
+                where S.code = {subject};
+            '''
+            res = self.db.exec(query).fetchall()
+
+            df = pd.DataFrame(
+                res, columns=['course offering name'])
+            print(df)
+        except mysql.connector.Error as error:
+            print(
+                f'Get course offerings by subject failed with error: {error}')
         return
 
     def do_get_course_details(self, arg):
-        'show the course\'s detail'
+        'show a course\'s detail'
 
         course_name = input('Enter the course name: ')
         try:
-            query = '''
+            query = f'''
             select
                 distinct C.name, I.name, CO.term_code
             from
@@ -52,16 +93,15 @@ class Education(cmd.Cmd):
                 inner join Sections AS S on CO.uuid = S.course_offering_uuid
                 inner join Teachings AS T on S.uuid = T.section_uuid
                 inner join Instructors AS I on T.instructor_id = I.id
-                where C.name = '{}';
-            '''.format(course_name)
+                where C.name = '{course_name}';
+            '''
             res = self.db.exec(query).fetchall()
 
             df = pd.DataFrame(
                 res, columns=['course name', 'instructor name', 'term code'])
-            print(df.to_string())
+            print(df)
         except mysql.connector.Error as error:
-            print('Get the course\'s detail failed with error: {}'.format(error))
-            self.db.rollback()
+            print(f'Get the course\'s detail failed with error: {error}')
 
     def do_show_all_instructors(self, arg):
         'show all instructors from UW Madison'
@@ -72,7 +112,12 @@ class Education(cmd.Cmd):
             df = pd.DataFrame(res, columns=['name'])
             print(df)
         except mysql.connector.Error as error:
-            print('Get all instructors failed with error: {}'.format(error))
+            print(f'Get all instructors failed with error: {error}')
+
+    def do_q(self, args):
+        'exit the program'
+
+        return True
 
 
 if __name__ == '__main__':
